a213e57fbec3a84643abaf13b7ea19156d82513a
Ivan Mazur <ipz221_miv@student.ztu.edu.ua>
2024-02-22 00:10:04 +0200

    Створив на C# консольний проєкт, який рахує суму двох чисел та виводить її на екран.

---
+++ Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double sum = num1 + num2;
+
+        Console.WriteLine($"The sum of the numbers: {sum}");
+
+        Console.ReadLine();
+    }
+}
